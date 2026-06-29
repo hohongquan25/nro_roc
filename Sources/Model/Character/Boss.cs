e580ba3ef62a19fccdb95c00ef25e0d085da4fc5
quanhh <quanhh@thudomultimedia.com>
2026-06-29 23:25:01 +0700

    x5 hp boss

--- Sources/Model/Character/Boss.cs
+++ Sources/Model/Character/Boss.cs
@@ -74,8 +74,8 @@ namespace NRO_Server.Model.Character
             Type = type;
             InfoChar.Stamina = bossTemplate.Stamina;
             InfoChar.MaxStamina = bossTemplate.Stamina;
-            InfoChar.OriginalHp = InfoChar.Hp = (long)bossTemplate.Hp * 10;
-            InfoChar.OriginalMp = InfoChar.Mp = (long)bossTemplate.Mp * 10;
+            InfoChar.OriginalHp = InfoChar.Hp = (long)bossTemplate.Hp * 5;
+            InfoChar.OriginalMp = InfoChar.Mp = (long)bossTemplate.Mp * 5;
             InfoChar.OriginalDamage = bossTemplate.Damage;
             InfoChar.OriginalDefence = bossTemplate.Defence;
             InfoChar.OriginalCrit = bossTemplate.CritChance;
